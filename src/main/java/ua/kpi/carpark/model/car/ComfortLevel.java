02ba6dceeb12d5aaa35ee09ed0731941c99ea7b8
mr-artur <ar2r.pv@gmail.com>
2020-02-24 23:34:33 +0200

    empty line in enum

--- src/main/java/ua/kpi/carpark/model/car/ComfortLevel.java
+++ src/main/java/ua/kpi/carpark/model/car/ComfortLevel.java
@@ -6,7 +6,6 @@ import java.math.BigDecimal;
  * Created by Artur Morozov on 2020-02-16
  */
 public enum ComfortLevel {
-
     ECONOMY(BigDecimal.valueOf(1)),
     STANDARD(BigDecimal.valueOf(1.2)),
     COMFORT(BigDecimal.valueOf(1.4)),
